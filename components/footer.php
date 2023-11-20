365ce119da1c0c6f4a98dc7156b5873e54e6c6ab
Dhairya-12 <chauhandhairya12@gmail.com>
2023-11-20 18:27:08 +0530

    first commit

---
+++ components/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>Our email</h3>
+         <a href="mailto:chauhandhairya12@gmail.com">chauhandhairya12@gmail.com</a>
+         <a href="deepakmevada6@gmail.com">deepakmevada6@gmail.com</a>
+         <a href="patel.harsh.1562@gmail.com">patel.harsh.1562@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>07:00am to 10:00pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">Mahemdabad, India - 387130</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>Contact Us</h3>
+         <a href="tel:1234567890">+91-8401563817</a>
+         <a href="tel:1112223333">+91-7046199542</a>
+         <a href="tel:1112223333">+91-8469372448</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; Capstone Project <?= date('Y'); ?> by <span> DDH </span></div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
